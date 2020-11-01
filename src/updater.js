a58fd360ea5b9252b986722ac7bbf5c20d42faa1
unknown <oleg.koleskin@gmail.com>
2020-11-01 18:46:40 +0200

    handling error in download

--- src/updater.js
+++ src/updater.js
@@ -6,7 +6,10 @@ autoUpdater.logger = require('electron-log');
 autoUpdater.logger.transports.file.level = 'info';
 
 module.exports = () => {
-  autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdatesAndNotify().catch((err) => {
+    dialog.showErrorBox('There was an error', `${err} occurred while trying to update your app`);
+    autoUpdater.logger.info(`There was an error with updating your app: ${err}`);
+  });
 
   autoUpdater.on('update-downloaded', () => {
     dialog.showMessageBox({
