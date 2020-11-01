fe37098c0b88e4a58eaaef48cdda03f55b4e5582
unknown <oleg.koleskin@gmail.com>
2020-11-01 18:29:34 +0200

    update-downloaded added

--- src/updater.js
+++ src/updater.js
@@ -1,4 +1,5 @@
 const { autoUpdater } = require('electron-updater');
+const { dialog } = require('electron');
 
 autoUpdater.logger = require('electron-log');
 
@@ -6,4 +7,18 @@ autoUpdater.logger.transports.file.level = 'info';
 
 module.exports = () => {
   autoUpdater.checkForUpdatesAndNotify();
+
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      title: 'Update downloaded',
+      type: 'info',
+      message: 'Install and restart now?',
+      buttons: ['Yes', 'Later'],
+    }).then((result) => {
+      const buttonIndex = result.response;
+      if (buttonIndex === 0) {
+        autoUpdater.quitAndInstall(false, true);
+      }
+    });
+  });
 };
